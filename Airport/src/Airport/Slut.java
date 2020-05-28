f69bf72cdd5ea1ec2bec78039b62a6c0808330a4
Tal Levin <talevin2000@gmail.com>
2020-05-28 12:28:04 +0300

    Hello

---
+++ Airport/src/Airport/Slut.java
@@ -0,0 +1,15 @@
+package Airport;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+import Airport.Flight.eStatus;
+
+public class Slut {
+
+	public static void main(String[] args) {
+		Duration a = Duration.ofMinutes(43643233);
+		System.out.println(a.toDays());
+	}
+
+}
